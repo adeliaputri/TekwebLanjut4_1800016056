22f94a9ed4df296729c3b16f9c4af1f24870c728
adelia putri <adelia1800016056@webmail.uad.ac.id>
2020-07-09 22:15:49 +0700

    first commit

---
+++ application/views/welcome_message.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome to my web</title>
+	<h2>Selamat anda berhasil Login</h2>
+	
+</head>
+<body>
+
+</body>
+</html>
